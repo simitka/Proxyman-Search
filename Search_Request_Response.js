28e201ba1345ff09466ee8260f858db3b842bf07
Nikita <60274458+Simitka@users.noreply.github.com>
2022-08-03 11:43:41 +0400

    v4
    - Add search in request / response / request+response

--- Search_Request_Response.js
+++ Search_Request_Response.js
@@ -1,47 +1,92 @@
-let color = {
-  red: {
-    whatFind: '',
-    exactSearch: true
-  },
-  blue: {
-    whatFind: '',
-    exactSearch: true
-  },
-  green: {
-    whatFind: '',
-    exactSearch: true
-  },
-  yellow: {
-    whatFind: 'Impression',
-    exactSearch: true
-  },
-  purple: {
-    whatFind: 'click',
-    exactSearch: false
-  },
-  gray: {
-    whatFind: '\"Gandalf Spot',
-    exactSearch: true
-  }
-};
+  function onResponse(context, url, request, response) {
+    ///////////////////////////////////////////// НАСТРОЙКА ПОИСКА ////////////////////////////////////////////
+    //В блоке color ниже можно выполнить поиск до 6 фраз и подсветить их разными цветами
+    //Для подсветки доступны 6 цветов: red, blue, green, yellow, purple, gray
+    //У каждого цвета есть параметры поиска, о них ниже:
+    ////whatFind – та фраза, которую будет искать скрипт
+    ////exactSearch – ВКЛ/ВЫКЛ жесткий поиск. В мягком поиске не учитывается регистр и ищутся фразы внутри фраз
+    ////whereSearch – искать внутри request / response / everywhere (request+response)
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var color = {
+      red: {
+        whatFind: '',
+        exactSearch: true,
+        whereSearch: 'request'
+      },
+      blue: {
+        whatFind: '',
+        exactSearch: true,
+        whereSearch: 'response'
+      },
+      green: {
+        whatFind: '"IP WW NA SHOP ALL OFFERS 17"',
+        exactSearch: true,
+        whereSearch: 'response'
+      },
+      yellow: {
+        whatFind: 'Impression',
+        exactSearch: true,
+        whereSearch: 'request'
+      },
+      purple: {
+        whatFind: '"model": "click"',
+        exactSearch: false,
+        whereSearch: 'request'
+      },
+      gray: {
+        whatFind: '"result": true',
+        exactSearch: false,
+        whereSearch: 'everywhere'
+      }
+    };
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function onRequest(context, url, request) {
-  for (var key in color) {
-    switch (color[key].exactSearch) {
-      case true:
-        var strJson = JSON.stringify(request.body).split(':').join(': ');
-        var regEx = new RegExp('(\\W|^)(' + color[key].whatFind + ')(\\W|$)');
-        break;
-      case false:
-        var strJson = JSON.stringify(request.body).split(':').join(': ').split('\"').join('');
-        var regEx = new RegExp(color[key].whatFind, "i");
-        break;
+    for (var key in color) {
+      if (color[key].whatFind != "") {
+        if (searchFun(color[key].whatFind, color[key].whereSearch, color[key].exactSearch != -1) {
+            response.color = key;
+            response.comment = color[key].whatFind;
+            return response;
+          }
+        }
+      }
+
+      return response;
+
+      ///////////////////////////////// FUNCTION /////////////////////////////////
+
+      function searchFun(whatFindValue, whereSearchValue, exactSearchValue) {
+        switch (whereSearchValue) {
+          case "request": {
+            var strJson = request.body;
+            break;
+          };
+        case "response": {
+          var strJson = response.body;
+          break;
+        };
+        case "everywhere": {
+          var strJson = Object.assign({}, request.body, response.body);
+          break;
+        };
+        };
+
+        strJson = JSON.stringify(strJson).split(':').join(': ').split(',').join(', ');
+
+        switch (exactSearchValue) {
+          case true:
+            var regEx = new RegExp('(\\W|^)(' + whatFindValue + ')(\\W|$)');
+            console.log("\n=============================\nTRUE | regEX = " + regEx);
+            console.log("TRUE | value = " + strJson + "\n=============================\n");
+            break;
+          case false:
+            var regEx = new RegExp(whatFindValue, "i");
+            console.log("\n=============================\nFALSE | regEX = " + regEx);
+            console.log("FALSE | value = " + strJson + "\n=============================\n");
+            break;
+        }
+
+        return strJson.search(regEx);
+
+      }
     }
-    if (color[key].whatFind != "" && strJson.search(regEx) != -1) {
-      console.log("\n===НАШЕЛ \'" + color[key].whatFind + "\', КРАШУ В " + key + " ===\n");
-      request.comment = color[key].whatFind
-      request.color = key
-      return request
-    }
-  }
-}
